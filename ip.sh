9b270f051b4f5efcf3e0f9b7cc3fd2d178814cc3
Sina-MD <sina.madmoli@gmail.com>
2025-03-05 22:08:00 +0330

    ip.sh

--- ip.sh
+++ ip.sh
@@ -1,44 +1,25 @@
 #!/bin/bash
-
 read -p "🔍 Enter IP: " IP
 if [ -z "$IP" ]; then
     echo -e "\e[1;32m❌ IP not entered, operation canceled.\e[0m"
     exit 1
 fi
-
 echo -e "\e[1;36m✅ R1Scanner Report for $IP\e[0m"
 echo "------------------------------------"
-
-# Ping
 echo -e "\e[1;5m[+] Ping\e[0m"
 ping -c 4 $IP
-
-# Netcat
 echo -e "\e[1;5m[+] Netcat Port 443\e[0m"
 nc -zv $IP 443
-
-# Telnet
 echo -e "\e[1;5m[+] Telnet Port 443\e[0m"
 (echo quit | telnet $IP 443 2>&1) | head -n 10
-
-# MTR بدون TCP
 echo -e "\e[1;5m[+] MTR (ICMP)\e[0m"
 mtr -n --report-wide -c 10 $IP
-
-# MTR با TCP Port 443
 echo -e "\e[1;5m[+] MTR (TCP Port 443)\e[0m"
 mtr -P 443 -n --report-wide -c 10 $IP
-
-# Nmap
 echo -e "\e[1;5m[+] Nmap Port 443\e[0m"
 nmap -p 443 $IP
-
-# Curl
 echo -e "\e[1;5m[+] Curl Header\e[0m"
 curl -I -m 10 https://$IP 2>/dev/null
-
-# OpenSSL
 echo -e "\e[1;5m[+] OpenSSL Certificate\e[0m"
 echo | openssl s_client -connect $IP:443 -servername $IP 2>/dev/null | openssl x509 -noout -dates
-
 echo -e "\e[1;32m✅ Report Completed\e[0m"
